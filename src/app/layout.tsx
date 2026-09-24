e86fc7a08296836ee16c241ad8686c2a83f1c3a8
faiq-dev2 <faiq.dev2@gmail.com>
2026-09-25 00:41:45 +0500

    add tawk to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Footer } from "@/components/home/Footer";
 import { Header } from "@/components/home/Header";
 import { ToastProvider } from "@/components/ui/Toast";
@@ -30,6 +31,20 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         />
       </head>
       <body className="min-h-full flex flex-col bg-background font-body-md text-body-md text-on-surface antialiased">
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+              var s1 = document.createElement("script");
+              var s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/6ab5797a83e52934410454c7/1k3ae5271";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
         <ToastProvider>
           <Header />
           {children}
